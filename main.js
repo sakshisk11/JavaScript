fea53785f0d28ac372a9f03cc1dad5d339e5d280
Sakshi SK <sakshikamaradgi11@gmail.com>
2024-08-11 16:08:37 +0530

    trial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+let array = [1,4,23,6,56];
+let i = 0;
+console.log("Numbers before reversing are:");
+for( i = 0;i < array.length;i++){
+    console.log(array[i]);
+}
+console.log("Numbers after reversing are:");
+for(i=array.length-1;i >=0;i--){
+    console.log(array[i]);
+}
+
+
+
+
